4d140aa37a62bb4aa6e87f88f3a251dc568e9c7b
yt-feng <39289088+yt-feng@users.noreply.github.com>
2026-07-04 17:19:23 +0800

    Allow Pages health checks

--- api/health.js
+++ api/health.js
@@ -1,4 +1,12 @@
-module.exports = function handler(_req, res) {
+module.exports = function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.setHeader("Cache-Control", "no-store");
